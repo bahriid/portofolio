cd729936320e44acf3413b0af399c66b7a4d4e09
Bahri Bahri <aribahri15@gmail.com>
2025-05-04 20:19:15 +0700

    CLEANup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     <title>Bahri</title>
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="porto/imgs/favicon.ico">
+{{--    <link rel="shortcut icon" href="porto/imgs/favicon.ico">--}}
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&amp;display=swap"
